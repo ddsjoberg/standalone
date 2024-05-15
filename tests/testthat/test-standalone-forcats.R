3644cba70b7853d1141d9f18f084e8dfa08aab6b
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-05-15 17:13:00 -0400

    forcats function tests

--- tests/testthat/test-standalone-forcats.R
+++ tests/testthat/test-standalone-forcats.R
@@ -1,7 +1,20 @@
 test_that("fct_infreq() works", {
+  f <- factor(c("b", "b", "a", "c", "c", "c"))
 
+  expect_equal(levels(fct_infreq(f)), as.character(unique(sort(f, decreasing = TRUE))))
+  expect_true(is.ordered(fct_infreq(f, ordered = TRUE)))
 })
 
 test_that("fct_inorder() works", {
+  f <- factor(c("b", "b", "a", "c", "c", "c"))
+
+  expect_equal(levels(fct_inorder(f)), as.character(unique(f)))
+  expect_true(is.ordered(fct_inorder(f, ordered = TRUE)))
+})
+
+test_that("fct_rev() works", {
+  f <- factor(c("b", "b", "a", "c", "c", "c"))
+
+  expect_equal(levels(fct_rev(f)), rev(levels(f)))
 
 })
