45dfcbf57930d81e27322fb389159b8ed46460db
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2024-05-27 20:34:23 -0400

    stringr func tests

--- tests/testthat/test-standalone-stringr.R
+++ tests/testthat/test-standalone-stringr.R
@@ -20,10 +20,10 @@ test_that("str_squish() works", {
   expect_equal(s, c("one 1", "two 2", "three 3"))
 
   s_notfixed <- str_squish("\n\nString with excess  trailing and leading white   space\n\n")
-  expect_equal(s, "String with excess trailing and leading white space")
+  expect_equal(s_notfixed, "String with excess trailing and leading white space")
 
   s_fixed <- str_squish("\n\nString with excess  trailing and leading white   space\n\n", fixed = TRUE)
-  expect_equal(s, "String with excess  trailing and leading white   space")
+  expect_equal(s_fixed, "String with excess  trailing and leading white   space")
 })
 
 test_that("str_remove_all() works", {
@@ -34,16 +34,40 @@ test_that("str_remove_all() works", {
   expect_equal(s, c("one1", "two2", "three3"))
 
   s_notfixed <- str_remove_all(c("one.1", "two..2", "three...3"), ".")
-  expect_equal(s, c("", "", ""))
+  expect_equal(s_notfixed, c("", "", ""))
 
   s_fixed <- str_remove_all(c("one.1", "two..2", "three...3"), ".", fixed = TRUE)
-  expect_equal(s, c("one1", "two2", "three3"))
+  expect_equal(s_fixed, c("one1", "two2", "three3"))
 })
 
 test_that("str_extract() works", {
+  shopping_list <- c("apples x43", "bag of flour", "bag of sugar", "milk x2")
 
+  s <- str_extract(shopping_list, "[a-z]+")
+  expect_equal(s, c("apples", "bag", "bag", "milk"))
+
+  s <- str_extract(shopping_list, "([a-z]+) of ([a-z]+)")
+  expect_equal(s, c(NA, "bag of sugar", "bag of flour", NA))
+
+  s_notfixed <- str_extract(shopping_list, "\\d")
+  expect_equal(s_notfixed, c("4", NA, NA, "2"))
+
+  s_fixed <- str_extract(shopping_list, "\\d", fixed = TRUE)
+  expect_equal(s_fixed, rep(NA_character_, 4))
 })
 
 test_that("str_detect() works", {
+  fruits <- c("apple", "banana", "pear", "pineapple")
 
+  s <- str_detect(fruits, "apple")
+  expect_equal(s, c(TRUE, FALSE, FALSE, TRUE))
+
+  s <- str_detect(fruits, "p")
+  expect_equal(s, c(TRUE, FALSE, TRUE, TRUE))
+
+  s_notfixed <- str_detect(fruits, "^a")
+  expect_equal(s_notfixed, c(TRUE, rep(FALSE, 3)))
+
+  s_fixed <- str_detect(fruits, "^a", fixed = TRUE)
+  expect_equal(s_fixed, rep(FALSE, 4))
 })
